96017937e92ecab72e6fe7e65d1f26a2ae0558e2
Matheus Andrade <matheus.amazonas@gmail.com>
2016-11-17 20:17:42 +0100

    Account Management Tests

--- tests/run.py
+++ tests/run.py
@@ -2,7 +2,7 @@
 import unittest
 import interface as i
 import test_cases 
-from test_cases import GetVariableTestCase
+from test_cases import GetVariableTestCase, AccountTestCase
 
 if __name__ == '__main__':
 	unittest.main()
