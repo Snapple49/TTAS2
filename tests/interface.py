13d67c9f6c937f2ff73cf2ba8c040e4c1edf067c
Matheus Andrade <matheus.amazonas@gmail.com>
2016-11-18 09:59:29 +0100

    Article Registration Tests
    Also, modified the flush_line method to a more generic flush_lines that will try to flush L lines. We need this because some commands (like ART_REG) return more than 1 line when executed successfully.

--- tests/interface.py
+++ tests/interface.py
@@ -1,3 +1,4 @@
+import re
 import telnetlib
 
 host = "sid.cs.ru.nl"
@@ -7,39 +8,40 @@ tn = telnetlib.Telnet(host, port)
 
 def sign_on(id, password):
 	tn.write('signon %s:%s\n' % (id,password)) 
-	return flush_line()
+	return flush_lines()
 
 def sign_off():
 	tn.write('signoff\n')
-	return flush_line()
+	return flush_lines()
 
 def get_variable(variable):
 	tn.write('get %s\n' % variable)
-	return flush_line()
+	return flush_lines()
 
 def art_id(barcode):
 	tn.write('artid %s\n' % barcode)
-	return flush_line()
+	return flush_lines()
 
 def art_reg(barcode, amount=1):
 	tn.write('artreg %s:%s\n' % (barcode, amount))
-	return flush_line()
+	# ARTEREG returns 2 lines so we need to flush 2 lines
+	return flush_lines(2)
 
 def trans(method, amount=""):
 	tn.write('trans %s %s\n' % (method, amount))
-	return flush_line()
+	return flush_lines()
 
 def idle():
 	tn.write('idle\n')
-	return flush_line()
+	return flush_lines()
 
 def open_acc(acc=""):
 	tn.write('open %s\n' % acc)
-	return flush_line()
+	return flush_lines()
 
 def close_acc():
 	tn.write('close\n')
-	return flush_line()
+	return flush_lines()
 
 def acc_state():
 	return get_variable('CS_ACCNT')
@@ -47,10 +49,20 @@ def acc_state():
 def quit():
 	tn.write('quit\n')
 
-def flush_line():
-	return tn.read_until('\n',2)
+def flush_lines(l = 1):
+	"""
+	Tries to flush l lines and returns the response
+	If the ReGex doesn't match, it returns the text received 
+	so far. 
+	"""
+	rgx = re.compile(r'(?:.+\n){%s}' % l)
+	(_, _, response) = tn.expect([rgx], 2)
+	return response
 
-def flush():
+def flush_all():
+	"""
+	Read everything until EOF. Blocks I/O until EOF is found.
+	"""
 	return tn.read_all()
 
 
--- tests/test_cases.py
+++ tests/test_cases.py
@@ -18,7 +18,7 @@ def up_to_idle():
 class BaseTestCase(unittest.TestCase):
 
 	def setUp(self):
-		i.flush_line()
+		i.flush_lines()
 		i.sign_on(8,"01")
 
 	def tearDown(self):
@@ -27,15 +27,14 @@ class BaseTestCase(unittest.TestCase):
 
 class OpenedAccountTestCase(BaseTestCase):
 
-	def __init__(self, id):
-		self.id = id;
-	
 	def setUp(self):
 		super(OpenedAccountTestCase, self).setUp()
 		i.open_acc()
 
 	def tearDown(self):
 		i.close_acc()
+		i.trans('TM_BANK')
+		i.idle()
 		super(OpenedAccountTestCase, self).tearDown()
 
 class GetVariableTestCase(BaseTestCase):
@@ -49,7 +48,6 @@ class GetVariableTestCase(BaseTestCase):
 	def test_si_scrp(self):
 		variable = i.get_variable('SI_SCRP')
 		return self.assertEqual(variable, "210 SI_SCRP:2.1\n")
-
 
 class AccountTestCase(BaseTestCase):
 
@@ -111,5 +109,38 @@ class TransactionsTestCase(OpenedAccountTestCase):
 		i.close_acc()
 		response = i.trans('TM_BANK')
 		return self.assertEqual(response, "240 0,00 Transaction succeeded\n")
+
+class ArtRegTestCase(OpenedAccountTestCase):
+
+	# Test 16
+	def test_no_amount(self):
+		expected = "212 Multifilter kek:6,99\n232 1: 6,99 Article registered\n"
+		response = i.art_reg(59922827) 
+		return (self.assertEqual(expected, response))  
+
+	# Test 17
+	def test_with_amount(self):
+		expected = "212 Multifilter kek:20,97:3\n232 3: 20,97 Article registered\n"
+		response = i.art_reg(59922827, 3)
+		return (self.assertEqual(expected, response))
+
+	# Test 18
+	def test_neg_amount(self):
+		expected = "501 Syntax error\n"
+		response = i.art_reg(59922827, -2)
+		return (self.assertEqual(expected, response))
+
+	# Test 19
+	def test_float_amont(self):
+		expected = "501 Syntax error\n"
+		response = i.art_reg(59922827, 2.45)
+		return (self.assertEqual(expected, response))
+
+	# Test 20
+	def test_float_amont(self):
+		expected = "501 Syntax error\n"
+		response = i.art_reg(59922827, 0)
+		return (self.assertEqual(expected, response))
+
 
 
