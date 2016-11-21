a5fea9d89c5aa55b35c840f50687794de20ad637
Snapple49 <oliver-dot-stein@hotmail.com>
2016-11-21 12:00:40 +0100

    added all testclasses to run

--- tests/run.py
+++ tests/run.py
@@ -2,7 +2,7 @@
 import unittest
 import interface as i
 import test_cases 
-from test_cases import (TransactionsTestCase)
+from test_cases import (TransactionsTestCase, GetVariableTestCase, AccountTestCase, ArtRegTestCase)
 
 if __name__ == '__main__':
 	unittest.main()
